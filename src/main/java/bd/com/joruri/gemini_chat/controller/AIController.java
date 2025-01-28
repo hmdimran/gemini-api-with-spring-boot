f9d25fa94c00cbdcbe98c1e49141871b1683a145
hmdimran <developerhimran@gmail.com>
2025-01-28 16:45:23 +0600

    Api integration Done and Works Fine By Postman

--- src/main/java/bd/com/joruri/gemini_chat/controller/AIController.java
+++ src/main/java/bd/com/joruri/gemini_chat/controller/AIController.java
@@ -5,16 +5,19 @@ import java.util.Map;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/ask")
+@RequestMapping("/api")
 @AllArgsConstructor
 public class AIController {
 
   private final GeminiAnswerService geminiAnswerService;
+
+  @PostMapping("/ask")
   ResponseEntity<String> askQuestions(@RequestBody Map<String,String> payload){
         String question = payload.get("question");
         String answer = geminiAnswerService.getAnswer(question);
--- src/main/java/bd/com/joruri/gemini_chat/service/GeminiAnswerService.java
+++ src/main/java/bd/com/joruri/gemini_chat/service/GeminiAnswerService.java
@@ -1,7 +1,9 @@
 package bd.com.joruri.gemini_chat.service;
 
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class GeminiAnswerService {
@@ -11,7 +13,37 @@ public class GeminiAnswerService {
   @Value("${gemini.api.key}")
   private String geminiApiKey;
 
+  private final WebClient webClient;
+
+  public GeminiAnswerService(WebClient.Builder webClient) {
+    this.webClient = webClient.build();
+  }
+
   public String getAnswer(String question) {
-    return "";
+    Map<String ,Object> requestBody = Map.of("contents",new Object[]{
+        Map.of("parts",new Object[]{
+            Map.of("text",question)
+        })
+    });
+
+
+//    {
+//      "contents": [
+  //        {
+  //          "parts":[
+    //            {"text": "Do You Know About Profile in spring"}
+    //          ]
+  //        }
+//        ]
+//    }
+
+    String response = webClient.post().uri(
+        geminiApiUrl + geminiApiKey
+    ).header(
+        "Content-Type","application/json"
+    ).bodyValue(requestBody).retrieve().bodyToMono(String.class).block();
+
+
+    return response;
   }
 }
